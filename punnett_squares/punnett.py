7e425ef62f07016ab644c9d6bbe4a2a9fccb55bf
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-09-30 09:57:58 +0100

    Create initial CLI gene inputs and adjust the Punnett class

--- punnett_squares/punnett.py
+++ punnett_squares/punnett.py
@@ -1,7 +1,9 @@
 class Punnett:
 
-    def __init__(self, n_genes=1) -> None:
+    def __init__(self, mother:list, father:list, n_genes=1) -> None:
         self.n_genes = n_genes
+        self.mother = mother
+        self.father = father
 
 if __name__ == "__main__":
     pass

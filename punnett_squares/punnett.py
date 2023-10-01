e054fcfb71452e01eea5d90161fea51e5e0708d5
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-10-01 10:00:16 +0100

    Tweaks to Punnett class and first attempt at the punnett_multi function

--- punnett_squares/punnett.py
+++ punnett_squares/punnett.py
@@ -5,5 +5,13 @@ class Punnett:
         self.mother = mother
         self.father = father
 
-if __name__ == "__main__":
-    pass
+    def get_dominance_values(self):
+        self.m_recessive_alleles = {}
+        for gene in self.mother:
+            self.m_recessive_alleles[gene[0]] = gene[0].islower()
+            self.m_recessive_alleles[gene[1]] = gene[1].islower()
+        self.f_recessive_alleles = {}
+        for gene in self.mother:
+            self.f_recessive_alleles[gene[0]] = gene[0].islower()
+            self.f_recessive_alleles[gene[1]] = gene[1].islower()
+        return(self.m_recessive_alleles, self.f_recessive_alleles)
--- punnett_squares/punnett_generator.py
+++ punnett_squares/punnett_generator.py
@@ -3,10 +3,10 @@ import pandas as pd
 from gene_creation import get_genes, determine_dominance
 from punnett import Punnett
 
-def punnett_2x2 (mother:list, father:list):
-    base_allele = mother[0][0].lower()
-    mother_alleles = [mother[0][0], mother[0][1]]
-    father_alleles = [father[0][0], father[0][1]]
+def punnett_2x2 (mother:str, father:str):
+    base_allele = mother[0].lower()
+    mother_alleles = [mother[0], mother[1]]
+    father_alleles = [father[0], father[1]]
     combinations = []
     for m_allele in mother_alleles:
         for f_allele in father_alleles:
@@ -23,12 +23,25 @@ def punnett_2x2 (mother:list, father:list):
     punnett_df = pd.DataFrame(punnett_array, columns=mother_alleles, index=father_alleles)
     return(punnett_df)
 
+def punnett_multi (mother:list, father:list):
+    punnett_list = []
+    for i,pair in enumerate(mother):
+        punnett_list.append(punnett_2x2(pair,father[i]))
+    print(np.concatenate(punnett_list[0],punnett_list[i]))
+    # punnett_array = np.array([combinations[:2],combinations[2:]])
+    # punnett_df = pd.DataFrame(punnett_array, columns=mother_alleles, index=father_alleles)
+
 def main():
     n_genes, pairs = get_genes()
     gene_dict = determine_dominance(pairs)
     punnett_object = Punnett(gene_dict["mother"], gene_dict["father"], n_genes=n_genes)
-    basic_punnett = punnett_2x2(punnett_object.mother, punnett_object.father)
-    print(basic_punnett)
+    punnett_object.get_dominance_values()
+    if len(punnett_object.mother) == 1:
+        basic_punnett = punnett_2x2(punnett_object.mother[0], punnett_object.father[0])
+        print(basic_punnett)
+    else:
+        multi_punnett = punnett_multi(punnett_object.mother, punnett_object.father)
+        print(multi_punnett)
 
 if __name__ == "__main__":
     main()
